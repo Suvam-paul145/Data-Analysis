69135103f52f65a1a8da011410435e53354e90cc
Suvam-paul145 <suvampersonal555@gmail.com>
2025-07-10 11:05:04 +0530

    git init

---
+++ dictionary/dictionary-operations.py
@@ -0,0 +1,36 @@
+# # write a python code to find the average marks of each student from the given dictionary of five subjects 
+
+# obj = {
+#     "ayan" :  [50,40,90,89,100],
+#     "sayan" : [60,70,80,90,100],
+#     "suman" : [70,80,90,100,100],
+#     "aman" : [80,90,100,100,100],
+#     "raman" : [90,100,100,100,100]
+# }
+
+# for i in obj:
+#     marks = obj[i]
+#     avg = sum(marks) / len(marks)
+#     print(f"The average marks of {i} is {avg:.2f}")
+
+# # WAP to arrange names using dictionary in alphabetical order
+
+# obj = {
+#     "ayan" ,
+#     "sayan",
+#     "suman",
+#     "aman" ,
+#     "raman"
+# }
+
+# print(sorted(obj))
+
+# WAP to sum  names using dictionary in alphabetical order
+
+obj = {
+    "onion" : 100,
+    "potato": 30,
+    "diswasher" : 40 ,
+    
+}
+
